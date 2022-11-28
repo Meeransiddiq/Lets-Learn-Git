81618824609a9f5bcd50ec28140b6b11ff6c6207
Meeran Siddiqui <mee.sidd.2019@gmail.com>
2022-11-28 23:44:07 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sum{
+    public static void main(String args[]){
+        int a,b;
+        System.out.println("Enter your number:");
+        Scanner sc=new Scanner(System.in);
+                a=sc.nextInt();
+                b=sc.nextInt();
+        System.out.println("The sum of the two numbers is:"+(a+b));
+        
+    }
+}
