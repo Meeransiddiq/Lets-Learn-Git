ae3c0c6a46956d82918083964c3e7def88fe887a
Meeran Siddiqui <mee.sidd.2019@gmail.com>
2022-11-29 20:56:46 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Multiply {
+    public static void main(String args[]){
+        int a,b;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter value of a and b:");
+             a=sc.nextInt();
+             b=sc.nextInt();
+    System.out.println("The product of the two number is:"+(a*b));
+    
+    }
+    
+}
