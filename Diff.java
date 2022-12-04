b403ebb8670461cde703a53d7c07be31d14adc70
Meeran Siddiqui <mee.sidd.2019@gmail.com>
2022-12-04 21:31:27 +0530

    modified again Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,13 @@
-public class Diff {
-         
+//Difference of the two number is given below
+import java.util.Scanner;
+ class Diff {
+         public static void main(String args[]){
+            int a,b,c;
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the value of a and b:");
+                   a=sc.nextInt();
+                   b=sc.nextInt();
+                System.out.println("The sum of the two number is:"+(a-b));
+
+         }
 }
